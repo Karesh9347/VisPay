37b3b6af97e36619f0db55e07f934f1e38fd2e1d
Karesh9347 <161163501+Karesh9347@users.noreply.github.com>
2024-03-03 17:42:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,24 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const { checkStatus } = require('./status');
 const { initiatePayment } = require('./initiatePayment');
-const {Student} = require('./Model/studentSchema');
-const {storeStudent}=require("./Controller/student")
-const {transactions}=require("./Controller/transaction")
-const {StudentL}=require("./Controller/StudentL")
-const {verifyToken}=require("./Controller/jwt/middleWear")
-
+const { Student } = require('./Model/studentSchema');
+const { storeStudent } = require('./Controller/student');
+const { transactions } = require('./Controller/transaction');
+const { StudentL } = require('./Controller/StudentL');
+const { verifyToken } = require('./Controller/jwt/middleWear');
+const multer = require('multer');
+const storage = multer.memoryStorage(); 
+const upload = multer({ storage: storage });
+dotenv.config();
 const app = express();
-const port =process.env.PORT||3001;
+const port = process.env.PORT || 3001;
 const router = express.Router();
 
-dotenv.config();
 app.use(cors());
 app.use(express.json());
-const BASE_URL=process.env.BASE_URL
+
+const BASE_URL = process.env.BASE_URL;
+
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -34,9 +38,9 @@ mongoose
 
 router.post('/initiatePayment', initiatePayment);
 router.post('/api/status/:transactionId', checkStatus);
-router.post('/registration', storeStudent);
-router.post("/addTransaction/:rollNumber",transactions)
-router.post("/login",StudentL)
+router.post('/registration',upload.single("profileImage"), storeStudent);
+router.post('/addTransaction/:rollNumber', transactions);
+router.post('/login', StudentL);
 router.get('/user', verifyToken, async (req, res) => {
   try {
     const user = await Student.findById(req.user.id);
@@ -45,41 +49,58 @@ router.get('/user', verifyToken, async (req, res) => {
       return res.status(404).json({ message: 'User not found' });
     }
 
-    
     res.json(user);
   } catch (error) {
     console.error('Error fetching user:', error);
     res.status(500).json({ message: 'Internal Server Error' });
   }
 });
-router.get("/allTransactions",async(req,res)=>{
-  try{
-  const allTransactions=await Student.find({})
-  console.log(allTransactions)
-  return res.json(allTransactions)
-  }
-  catch(err){
-    console.log("error at",err)
-  }
-})
-router.delete("/deleteStudent",async(req,res)=>{
-  const {rollNumber}=req.body;
-  try{
-  const student=await Student.findOneAndDelete({rollNumber})
-  if(!student){
-    console.log("user not found")
-    return res.json({message:"user not found with this roll number"})
-  }
-  else{
-    return res.json({message:"user deleted success fully",user:student})
-  }
-  }
-  catch(error){
-    console.log(error)
-  }
-  
 
-})
+router.get('/allTransactions', async (req, res) => {
+  try {
+    const allTransactions = await Student.find({});
+   
+    return res.json(allTransactions);
+  } catch (err) {
+    console.log('error at', err);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+router.delete('/deleteStudent', async (req, res) => {
+  const { rollNumber } = req.body;
+  try {
+    const student = await Student.findOneAndDelete({ rollNumber });
+    if (!student) {
+      console.log('user not found');
+      return res.json({ message: 'User not found with this roll number' });
+    } else {
+      return res.json({ message: 'User deleted successfully' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+router.put('/updateStudent/:rollNumber', async (req, res) => {
+
+const {rollNumber}=req.params
+  try {
+    const student = await Student.findOneAndUpdate({ rollNumber },{$set:req.body},{new:true});
+    if (!student) {
+      console.log('user not found');
+      return res.json({ message: 'User not found with this roll number' });
+    } else {
+
+      return res.json({ message: 'User updated successfully' });
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+
 app.use('/', router);
 
 app.listen(port, () => {
