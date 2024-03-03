0230e2880ddb7d8b01cfd6c6c28c73525e6768c4
Karesh9347 <161163501+Karesh9347@users.noreply.github.com>
2024-03-03 19:19:16 +0530

    Update student.js

--- Controller/student.js
+++ Controller/student.js
@@ -1,19 +1,61 @@
 const express = require('express');
-const {Student} = require('../Model/studentSchema');
+const { Student } = require('../Model/studentSchema');
 
 const storeStudent = async (req, res) => {
-  const { name, rollNumber, mobileNumber,password ,admissionType,year,section,paidFee,totalFee,regulation,dob} = req.body;
-  if (!mobileNumber || !rollNumber || !name || !password || !admissionType || !year || !section || !paidFee || !totalFee || !regulation || !dob) {
+  const {
+    name,
+    rollNumber,
+    mobileNumber,
+    password,
+    admissionType,
+    year,
+    section,
+    paidFee,
+    totalFee,
+    regulation,
+    dob,
+    role,
+    address,
+    guardianName,
+    email,
+  } = req.body;
+
+  // Check if req.file is defined
+  if (!req.file) {
+    return res.status(400).json({ error: 'Image file is required' });
+  }
+
+  const { buffer, mimetype } = req.file;
+
+  if (
+    !mobileNumber ||
+    !rollNumber ||
+    !name ||
+    !password ||
+    !admissionType ||
+    !year ||
+    !section ||
+    !paidFee ||
+    !totalFee ||
+    !regulation ||
+    !dob ||
+    !role ||
+    !email ||
+    !address ||
+    !guardianName ||
+    !buffer ||
+    !mimetype
+  ) {
     console.log('User is not created');
     return res.status(400).json({ error: 'User is not created. Please provide valid data.' });
   }
-  
 
   try {
-  
     const newStudent = new Student({
       name,
       rollNumber,
+      role,
+      dob,
       mobileNumber,
       password,
       admissionType,
@@ -22,18 +64,33 @@ const storeStudent = async (req, res) => {
       paidFee,
       regulation,
       totalFee,
-      dob,
+      address,
+      email,
+      guardianName,
+      profileImage: {
+        data: buffer,
+        contentType: mimetype,
+      },
     });
-    const existStudent=await Student.findOne({rollNumber});
-    if(existStudent){
-      res.json({message:"user exist with this number"})
+
+    const existStudent = await Student.findOne({ rollNumber });
+
+    if (existStudent) {
+      return res.json({ message: 'User exists with this roll number' });
     }
-    
+
     await newStudent.save();
-    res.json({ message: 'User created successfully' });
+    return res.json({ message: 'User created successfully' });
   } catch (error) {
     console.error('Error creating user:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+
+    if (error.code === 11000 && error.keyPattern && error.keyValue) {
+      const duplicateField = Object.keys(error.keyPattern)[0];
+      const duplicateValue = error.keyValue[duplicateField];
+      return res.status(400).json({ error: `Duplicate key error: ${duplicateField} '${duplicateValue}' already exists.` });
+    }
+
+    return res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
