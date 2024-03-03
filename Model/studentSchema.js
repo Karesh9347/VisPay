1fcd168ea0801a84ef19a04226508f1c6c9ed1de
Karesh9347 <161163501+Karesh9347@users.noreply.github.com>
2024-03-03 19:18:25 +0530

    Update studentSchema.js

--- Model/studentSchema.js
+++ Model/studentSchema.js
@@ -1,4 +1,4 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
 const transactionSchema = new mongoose.Schema({
   transactionId: {
@@ -28,16 +28,69 @@ const studentSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  password:{
-    type: String,
-    required: true,
-
+  role:{
+type:String,
+required:true,
+  },
+  dob:{
+    type:Date,
+    required:true,
   },
   mobileNumber: {
     type: String,
     required:true,
   },
+  password:{
+    type: String,
+    required: true,
+
+  },
+  admissionType:{
+    type: String,
+    required:true,
+
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  section: {
+    type: String,
+    required: true,
+  },
+  paidFee:{
+    type: String,
+    required: true,
+
+  },
+  regulation:{
+type:String,
+required:true,
+  },
+  totalFee: {
+    type: String,
+    required:true,
+  },
+  address:{
+    type:String,
+    required:true,
+
+  },
+email:{
+    type:String,
+    required:true,
+  },
+  guardianName:{
+    type:String,
+    required:true,
+  },
+
   transactions: [transactionSchema],
+  profileImage: {
+    data: Buffer,
+    contentType: String,
+  },
+
 });
 
 const Student = mongoose.model("Student", studentSchema);
